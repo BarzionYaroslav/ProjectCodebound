753dc8c52d29f11d584583b92dbe4d79423f3ac6
BarzionYaroslav <deleted.2020@mail.ru>
2026-02-20 21:44:22 +0300

    Added UI testing

--- CodeboundCore/Program.cs
+++ CodeboundCore/Program.cs
@@ -9,6 +9,7 @@ class Program
 {
     static void Main()
     {
+        bool gameStopped = false;
         var audioFile = new AudioFileReader(@"./assets/15. Heian Alien.mp3");
         var outputDevice = new WaveOutEvent();
         outputDevice.Init(audioFile);
@@ -34,10 +35,11 @@ class Program
         int imageFrame = 0;
         int fps = 60;
         string text;
+        int choice = 0;
         Console.Clear();
         Console.CursorVisible = false;
         MagickImage stage;
-        while (true)
+        while (!gameStopped)
         {
             if (Console.KeyAvailable)
             {
@@ -46,6 +48,12 @@ class Program
                     fps -= 1;
                 if (key == ConsoleKey.RightArrow)
                     fps += 1;
+                if (key == ConsoleKey.UpArrow)
+                    choice -= 1;
+                if (key == ConsoleKey.DownArrow)
+                    choice += 1;
+                if (key == ConsoleKey.Escape)
+                    gameStopped = true;
                 if (key == ConsoleKey.P && outputDevice.PlaybackState != PlaybackState.Playing)
                     outputDevice.Play();
                 if (key == ConsoleKey.S && outputDevice.PlaybackState == PlaybackState.Playing)
@@ -53,9 +61,9 @@ class Program
             }
             text = "";
             stage = new MagickImage(imageFromFile[0]);
-            stage.Composite(enemyFromFile[(imageFrame / 5) % imageNum], 32, -6 + (int)(Math.Sin((MathF.PI / 180) * imageFrame*4) * 3), CompositeOperator.Over);
-            stage.Composite(arm1[0], -3 + (int)(Math.Cos((MathF.PI / 180) * imageFrame*5) * 3), -3+(int)(Math.Sin((MathF.PI / 180) * imageFrame*5)*2), CompositeOperator.Over);
-            stage.Composite(arm2[0], (90-28) - (int)(Math.Cos((MathF.PI / 180) * imageFrame*5) * 3), -3+(int)(Math.Sin((MathF.PI / 180) * imageFrame*5)*2), CompositeOperator.Over);
+            stage.Composite(enemyFromFile[(imageFrame / 5) % imageNum], 32, -6 + (int)(Math.Sin((MathF.PI / 180) * imageFrame * 4) * 3), CompositeOperator.Over);
+            stage.Composite(arm1[0], -3 + (int)(Math.Cos((MathF.PI / 180) * imageFrame * 5) * 3), -3 + (int)(Math.Sin((MathF.PI / 180) * imageFrame * 5) * 2), CompositeOperator.Over);
+            stage.Composite(arm2[0], (90 - 28) - (int)(Math.Cos((MathF.PI / 180) * imageFrame * 5) * 3), -3 + (int)(Math.Sin((MathF.PI / 180) * imageFrame * 5) * 2), CompositeOperator.Over);
             var pixels = stage.GetPixels();
             uint imageWidth = stage.Width;
             var imageHeight = Math.Min(stage.Height, Console.BufferHeight);
@@ -83,13 +91,80 @@ class Program
                 }
                 text += "\n";
             }
-            text += $"\e[1m{fps} Frames per Second | {Console.BufferWidth} width and {Console.BufferHeight} height";
-            text += $"\nIs music playing: {outputDevice.PlaybackState == PlaybackState.Playing}";
             Console.Write(text);
+            DrawUi((int)(imageWidth * 2 * 1 / 4), (int)(imageWidth * 2 * 3 / 4), (int)bufferSize[1] - (int)imageHeight,
+            [
+                "FIGHT",
+                "SPELL",
+                "INVENTORY",
+                "DEFEND",
+                "RUN"
+            ], choice, "PUNCHY BAD does nothing in particular. You feel threatened nonetheless.");
             Console.ResetColor();
             imageFrame++;
             Console.SetCursorPosition(0, 0);
             Thread.Sleep(1000 / fps);
         }
+    }
+
+    static void DrawUi(int width1, int width2, int height, List<string> words, int choice, string rtext)
+    {
+        string text = "";
+        int wrdCount = words.Count;
+        List<string> rlines = StringToLines(rtext, width2/2 - 4, (height - 4) / 2);
+        int linecount = rlines.Count;
+        for (int i = 0; i < height; i++)
+        {
+            string starter = (i < height - 2 && i % 2 == 0) || (i >= height - 2 && (height - 2 - i) % 2 == 0) ? "\e#3" : "\e#4";
+            string msg = " ";
+            string txt = "";
+            string col = "\e[0m";
+            if ((i - 2) / 2 < wrdCount && i > 1)
+            {
+                msg += words[(i - 2) / 2];
+            }
+            if ((i - 2) / 2 < linecount && i > 1)
+            {
+                txt += rlines[(i - 2) / 2];
+            }
+            if (choice == (i - 2) / 2)
+                col = "\e[38:5:220m";
+            if (i <= 1)
+                text += starter + "╔" + new string('═', width1 / 2 - 1) + "╦" + new string('═', width2 / 2 - 1) + "╗\n";
+            else if (i >= height - 2)
+                text += starter + "╚" + new string('═', width1 / 2 - 1) + "╩" + new string('═', width2 / 2 - 1) + "╝\n";
+            else
+                text += starter + "║" + col + msg.PadRight(width1 / 2 - 1) + "\e[0m║ " + txt.PadRight(width2 / 2 - 2) + "║\n";
+        }
+        Console.Write(text);
+    }
+
+    static List<string> StringToLines(string text, int size, int maxLines)
+    {
+        List<string> answer = new List<string>();
+        int left = 0;
+        int space;
+        while (answer.Count<maxLines)
+        {
+            if (text.Length - left <= size)
+            {
+                if (text[left] == ' ')
+                    answer.Add(text.Substring(left + 1));
+                else
+                    answer.Add(text.Substring(left));
+                break;
+            }
+            space = text.LastIndexOf(' ', left + size, size);
+            if (space == -1)
+            {
+                space = left + size;
+            }
+            if (text[left] == ' ')
+                answer.Add(text.Substring(left + 1, space - left));
+            else
+                answer.Add(text.Substring(left, space-left));
+            left = space;
+        }
+        return answer;
     }
 }
